f76fe74908b1366f982c48311781617dff81df1a
Ilja Semjonov <ilsemjonov@gmail.com>
2020-06-09 15:37:02 +0300

    password input

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -14,7 +14,7 @@ class LoginScreenState extends State<LoginScreen> {
         child: Column(
           children: [
             emailField(),
-            // passwordField(),
+            passwordField(),
             // submitButton(),
           ],
         ),
@@ -32,7 +32,15 @@ class LoginScreenState extends State<LoginScreen> {
     );
   }
 
-  Widget passwordField() {}
+  Widget passwordField() {
+    return TextFormField(
+      obscureText: true,
+      decoration: InputDecoration(
+        labelText: 'Password',
+        hintText: 'Password',
+      ),
+    );
+  }
 
   Widget submitButton() {}
 }
