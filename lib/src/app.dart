a1aded4c82cada9b9d03300709542d475dd7bb1e
Ilja Semjonov <ilsemjonov@gmail.com>
2020-06-09 15:02:19 +0300

    container added

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'screens/login_screen.dart';
 
 class App extends StatelessWidget {
   Widget build(context) {
     return MaterialApp(
       title: 'Log me in!',
       home: Scaffold(
-        body: Text('Show a form here!'),
+        body: LoginScreen(),
       ),
     );
   }
