aa2624f92eddeb95ff8faeecf6bf449c0e5ffecd
Ilja Semjonov <ilsemjonov@gmail.com>
2020-06-09 15:48:57 +0300

    raisedbutton

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -15,7 +15,7 @@ class LoginScreenState extends State<LoginScreen> {
           children: [
             emailField(),
             passwordField(),
-            // submitButton(),
+            submitButton(),
           ],
         ),
       ),
@@ -42,5 +42,10 @@ class LoginScreenState extends State<LoginScreen> {
     );
   }
 
-  Widget submitButton() {}
+  Widget submitButton() {
+    return RaisedButton(
+      child: Text('Submit'),
+      onPressed: () {},
+    );
+  }
 }
